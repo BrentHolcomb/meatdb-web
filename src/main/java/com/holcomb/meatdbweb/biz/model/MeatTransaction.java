2b27c78e1588cf992c0f973b027f98a05442de65
BrentHolcomb <brentjholcomb@gmail.com>
2022-11-21 00:22:57 -0500

    add non-negative validation

--- src/main/java/com/holcomb/meatdbweb/biz/model/MeatTransaction.java
+++ src/main/java/com/holcomb/meatdbweb/biz/model/MeatTransaction.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
-import javax.validation.constraints.Digits;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.PastOrPresent;
+import javax.validation.constraints.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
@@ -29,22 +26,27 @@ public class MeatTransaction {
     @Enumerated(EnumType.STRING)
     private MeatType type;
 
+    @PositiveOrZero(message = "Pounds of meat sold must be positive")
     @NotNull(message = "Pounds of meat sold must be entered")
     @Digits(integer=5, fraction=3, message = "Pounds must be less than 100,000 lb and have no more than 3 decimal places")
     private BigDecimal sold;
 
+    @PositiveOrZero(message = "Pounds of meat waste must be positive")
     @NotNull(message = "Pounds of meat wasted must be entered")
     @Digits(integer=5, fraction=3, message = "Pounds must be less than 100,000 lb and have no more than 3 decimal places")
     private BigDecimal waste;
 
+    @PositiveOrZero(message = "Pounds of meat purchased must be positive")
     @NotNull(message = "Pounds of meat purchased must be entered")
     @Digits(integer=5, fraction=3, message = "Pounds must be less than 100,000 lb and have no more than 3 decimal places")
     private BigDecimal purchase;
 
+    @PositiveOrZero(message = "Dollar sales for sold meat must be positive")
     @NotNull(message = "Dollar sales for sold meat must be entered")
     @Digits(integer=7, fraction=2, message = "Dollar value must be less than $10,000,000 and have no more than 2 decimal places")
     private BigDecimal sales;
 
+    @PositiveOrZero(message = "Dollar expense for purchased meat must be positive")
     @NotNull(message = "Dollar expense for purchased meat must be entered")
     @Digits(integer=7, fraction=2, message = "Dollar value must be less than $10,000,000 and have no more than 2 decimal places")
     private BigDecimal expense;
